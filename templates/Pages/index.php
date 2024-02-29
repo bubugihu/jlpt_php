66fe8c1bc783a3d94061f4ac4442ba37165f7a00
Hungbg <bubugihu@gmail.com>
2024-02-29 14:02:11 +0700

    remove acc

--- templates/Pages/index.php
+++ templates/Pages/index.php
@@ -56,8 +56,8 @@
     <link rel="stylesheet" href="assets/css/responsive.css?v=2">
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="https://mehedi.asiandevelopers.com/hoppex/assets/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="https://mehedi.asiandevelopers.com/hoppex/assets/images/favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="https://mehedi.asiandevelopers.com/hoppex/assets/images/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="assets/images/about/logo.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/images/about/logo.png" sizes="16x16">
 
 </head>
 
