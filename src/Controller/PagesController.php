0cc5e5196fac04d018e996e4622466a233f88ad8
bubugihu <bubugihu@gmail.com>
2025-03-01 17:32:40 +0700

    fix base url

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -50,11 +50,7 @@ class PagesController extends AppController
     {
         parent::initialize();
         $this->business_manage_system = new ManageSystem();
-        if (!isset($base_url) || strlen($base_url) == 0)
-        {
-            $base_url =  $this->request->getAttribute("webroot");
-        }
-        $this->set('base_url',$base_url);
+        $this->set('base_url',BASE_URL . "webroot");
     }
     public function index()
     {
